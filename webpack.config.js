89ebd91ad2d388b538fae7c7e12bce25deb22a7c
Taiwon Chung <tchung1118@gmail.com>
2018-01-19 22:17:49 +0900

    index now rendering via React

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'app/client/publish');
+var APP_DIR = path.resolve(__dirname, 'app/client/src');
+
+var config = {
+  entry: APP_DIR + '/index.js',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }/*,
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }*/
+    ]
+  }
+};
+
+module.exports = config;
